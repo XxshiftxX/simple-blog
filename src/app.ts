ca162770c2d5f916fcbdace3ea253bb352d424f0
shift <qjfrntop12@naver.com>
2019-10-11 23:35:32 +0900

    에러 핸들링 구현

---
+++ src/app.ts
@@ -0,0 +1,29 @@
+import * as express from 'express'
+
+type Request = express.Request
+type Response = express.Response
+type NextFunc = express.NextFunction
+
+interface HttpError extends Error {
+  status: number
+  data?: any
+}
+
+export const app = express()
+
+app.get('/', (req: Request, res: Response) => {
+  res.end('Hello world!')
+})
+
+app.use((req: Request, res: Response, next: NextFunc) => {
+  let error = new Error('Not Found') as HttpError
+  error.status = 404
+  next(error)
+})
+
+app.use((err: HttpError, req: Request, res: Response, next: NextFunc) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+    data: err.data
+  })
+})
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,4 @@
-import * as express from 'express'
-
-const app = express()
-
-app.get('/', (req: express.Request, res: express.Response) => {
-  res.end('Hello world!')
-})
+import { app } from './app'
 
 app.listen(3000, () => {
   console.log('Server listening on port 3000!')
