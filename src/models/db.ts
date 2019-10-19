7961b1c8d25b0ea16e423f9c9af2484ef4110a0f
shift <qjfrntop12@naver.com>
2019-10-19 17:18:55 +0900

    CRUD를 위한 DB 클래스 생성

--- src/models/db.ts
+++ src/models/db.ts
@@ -1,4 +1,7 @@
-import { connect, connection } from 'mongoose'
+import { connect, connection, Document } from 'mongoose'
+
+import { User, UserModel } from './user'
+import { PostModel, Post } from './post'
 
 const connectDB = () => {
   const option = {
@@ -16,3 +19,43 @@ const connectDB = () => {
 
 connectDB()
 connection.on('disconnected', connectDB)
+
+export class UserDB {
+  create (user: User) {
+    const model = new UserModel(user)
+    return model.save()
+  }
+
+  find (query: any) {
+    return UserModel.find(query)
+  }
+
+  update (user: User) {
+    const { email } = user
+    return UserModel.update({ email }, { ...user })
+  }
+
+  delete (user: User) {
+    const { email } = user
+    return UserModel.remove({ email })
+  }
+}
+
+export class PostDB {
+  create (post: Post) {
+    const model = new PostModel(post)
+    return model.save()
+  }
+
+  find (query: any) {
+    return UserModel.find(query)
+  }
+
+  update (post: Post, id: number) {
+    return UserModel.update({ id }, { ...post })
+  }
+
+  delete (id: number) {
+    return UserModel.remove({ id })
+  }
+}
