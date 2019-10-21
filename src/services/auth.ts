244e883d24ce3c7298706fec280747f25b2e105b
shift <qjfrntop12@naver.com>
2019-10-21 17:13:56 +0900

    [#13] POST /login 구현

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from 'express'
 import { createHmac } from 'crypto'
+import { sign } from 'jsonwebtoken'
 
 import * as config from '../config'
 import { User, UserDB } from '../models/user'
@@ -28,6 +29,13 @@ const checkUserDuplicated = async (email: string) => {
   return false
 }
 
+const verifyPassword = (password: string, input: string) => {
+  const encrypted = encryptValue(input)
+  return password === encrypted
+}
+
+const getJWTToken = (payload: any) => sign(payload, config.secret, { expiresIn: '7d', issuer: 'dev-shift.me' })
+
 export const signUp = async (req: Request, res: Response, next: NextFunction) => {
   if (await checkUserDuplicated(req.body.email)) {
     next(new HttpError(400, 'user already exist', { data: req.body }))
@@ -43,11 +51,37 @@ export const signUp = async (req: Request, res: Response, next: NextFunction) =>
 
 export const duplicated = async (req: Request, res: Response, next: NextFunction) => {
   const { email } = req.query
+
   if (!email) {
-    next(new HttpError(400, 'no email'))
+    next(new HttpError(400, 'request data missing'))
     return
   }
 
   const result = await checkUserDuplicated(email)
   res.json({ email, result })
+}
+
+export const login = async  (req: Request, res: Response, next: NextFunction) => {
+  const { email, password } = req.body
+  if (!email || !password) {
+    next(new HttpError(400, 'request data missing'))
+    return
+  }
+
+  const [ user ] = await UserDB.find({ email })
+  if (!user) {
+    next(new HttpError(404, 'user not found'))
+    return
+  }
+  if (!verifyPassword(user.password, password)) {
+    next(new HttpError(401, 'wrong password'))
+    return
+  }
+
+  const payload = {
+    _id: user._id,
+    email: user.email
+  }
+  const token = getJWTToken(payload)
+  res.json({ data: { token } })
 }
