5ed937f040ff90babf380b41d0cc7cc221a00cab
shift <qjfrntop12@naver.com>
2019-10-23 00:57:06 +0900

    [#5] POST /auth/login 테스트 작성

--- test/routes/auth.test.ts
+++ test/routes/auth.test.ts
@@ -1,4 +1,5 @@
 import * as request from 'supertest'
+import * as jwt from 'jsonwebtoken'
 import * as sinon from 'sinon'
 import { expect } from 'chai'
 import { Hmac } from 'crypto'
@@ -6,12 +7,15 @@ import { Hmac } from 'crypto'
 import app from '../../src/app'
 import { UserDB, User } from '../../src/models/user'
 
+type jwtStubType = sinon.SinonStub<[string | Buffer | object, jwt.Secret, jwt.SignOptions], string>
+
 describe('/auth route test', () => {
   const req = request(app)
 
   const userCreateStub = sinon.stub(UserDB, 'create')
   const userFindStub = sinon.stub(UserDB, 'find')
   const encrypteStub = sinon.stub(Hmac.prototype, 'digest')
+  const jwtStub = sinon.stub(jwt, 'sign')
 
   beforeEach(() => {
     sinon.reset()
@@ -68,6 +72,50 @@ describe('/auth route test', () => {
 
     it('400 Bad Request (request data missing)', async () => {
       await req.get('/api/auth/duplicated').expect(400)
+    })
+  })
+
+  describe('POST /auth/login', () => {
+    beforeEach(() => {
+      jwtStub.value((payload: any) => JSON.stringify(payload))
+      encrypteStub.returns('encrypted')
+    })
+
+    it('200 OK', async () => {
+      userFindStub.resolves([{ email: 'test', password: 'encrypted' } as User])
+
+      const res = await req.post('/api/auth/login').send({ email: 'test', password: 'test' }).expect(200)
+      const expectedToken = JSON.stringify({ email: 'test' })
+
+      expect(res.body).to.eql({ data: { token: expectedToken } })
+    })
+
+    it('400 Bad Request (email missing)', async () => {
+      const res = await req.post('/api/auth/login').send({ password: 'test' }).expect(400)
+
+      expect(res.body.message).to.eql('request data missing')
+    })
+
+    it('400 Bad Request (password missing)', async () => {
+      const res = await req.post('/api/auth/login').send({ email: 'test' }).expect(400)
+
+      expect(res.body.message).to.eql('request data missing')
+    })
+
+    it('401 Unauthorized', async () => {
+      userFindStub.resolves([{ email: 'test', password: 'wrongpassword' } as User])
+
+      const res = await req.post('/api/auth/login').send({ email: 'test', password: 'test' }).expect(401)
+ 
+      expect(res.body.message).to.eql('wrong password')
+    })
+
+    it('404 Not Found', async () => {
+      userFindStub.resolves([])
+
+      const res = await req.post('/api/auth/login').send({ email: 'test', password: 'test' }).expect(404)
+
+      expect(res.body.message).to.eql('user not found')
     })
   })
 })
