4d55f3b03dff753a1352ca931e4f5f65cf3fb611
shift <qjfrntop12@naver.com>
2019-10-22 15:44:25 +0900

    [#5] GET /auth/duplicted 테스트 작성

--- test/routes/auth.test.ts
+++ test/routes/auth.test.ts
@@ -10,11 +10,11 @@ describe('/auth route test', () => {
   const req = request(app)
   const sandbox = sinon.createSandbox()
 
-  describe('GET /auth/signup', () => {
-    const userCreateStub = sandbox.stub(UserDB, 'create')
-    const userFindStub = sandbox.stub(UserDB, 'find')
-    const encrypteStub = sandbox.stub(Hmac.prototype, 'digest')
+  const userCreateStub = sandbox.stub(UserDB, 'create')
+  const userFindStub = sandbox.stub(UserDB, 'find')
+  const encrypteStub = sandbox.stub(Hmac.prototype, 'digest')
 
+  describe('GET /auth/signup', () => {
     beforeEach(() => {
       sandbox.reset()
 
@@ -38,13 +38,39 @@ describe('/auth route test', () => {
       expect(userCreateStub.args[0]).to.eql(expected)
     })
 
-    it('500 E-Mail Duplicated', async () => {
+    it('400 Bad Request (email duplicated)', async () => {
       userFindStub.resolves([{ email: 'testem', password: 'testpw' } as User])
 
       const data = { email: 'testem', password: 'testpw' }
       const res = await req.post('/api/auth/signup').send(data).expect(400)
 
       expect(res.body.message).to.eql('user already exist')
+    })
+  })
+
+  describe('GET /auth/duplicated', () => {
+    beforeEach(() => {
+      sandbox.reset()
+    })
+
+    it('200 OK (duplicated)', async () => {
+      userFindStub.resolves([{ email: 'test', password: 'test' } as User])
+
+      const res = await req.get('/api/auth/duplicated?email=test').expect(200)
+
+      expect(res.body).to.eql({ email: 'test', result: true })
+    })
+
+    it('200 OK (not duplicated)', async () => {
+      userFindStub.resolves([])
+
+      const res = await req.get('/api/auth/duplicated?email=test').expect(200)
+
+      expect(res.body).to.eql({ email: 'test', result: false })
+    })
+
+    it('400 Bad Request (request data missing)', async () => {
+      await req.get('/api/auth/duplicated').expect(400)
     })
   })
 })
